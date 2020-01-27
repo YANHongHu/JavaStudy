07690077bcaa178820973798b970d65539fa704a
鸿鹄~ <49400532+YANHongHu@users.noreply.github.com>
2020-01-28 00:37:04 +0800

    更新

--- JDBCstudy/src/JDBCSelectpractice.java
+++ JDBCstudy/src/JDBCSelectpractice.java
@@ -5,8 +5,14 @@ public class JDBCSelectpractice {
 //        int x=20,y=5;
 //        System.out.println(x+y+""+(x+y)+y);
         Scanner sc = new Scanner(System.in);
-        String str = sc.nextLine();
-        System.out.println(toLowerCase(str));
+        //String str = sc.nextLine();
+        //System.out.println(toLowerCase(str));
+        int[] nums = {1,34,3,5,4};
+        int k = sc.nextInt();
+        rotate(nums,k);
+        for (int i : nums) {
+            System.out.println(i);
+        }
     }
 
     public static String toLowerCase(String str){
@@ -18,5 +24,17 @@ public class JDBCSelectpractice {
             str_n.append(ch);
         }
         return str_n.toString();
+    }
+
+    public static void rotate(int[] nums, int k){
+        int len = nums.length;
+        for(int i=0;i<k;i++) {
+            int a = 0;
+            a = nums[len-1];
+            for(int j = len-2;j>=0;j--){
+                nums[j+1]=nums[j];
+            }
+            nums[0] = a;
+        }
     }
 }
